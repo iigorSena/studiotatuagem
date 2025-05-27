e360f474e7283d4d12ea04efa73917fc472291bc
Igor Sena de Sousa <136903697+iigorSena@users.noreply.github.com>
2025-05-27 19:22:55 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log("Site Studio Ouro Ink carregado com sucesso.");
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+    target.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    });
+    }
+});
+});
